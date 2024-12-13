463e795691f975ccf1599aa1ab2733f695af98c2
brickandmorty <01310623@edu.campus02.at>
2024-12-13 15:00:59 +0100

    Fixed Typing Error in Parser
    closes #1

--- src/main/java/at/campus02/swe/logic/CalculatorImpl.java
+++ src/main/java/at/campus02/swe/logic/CalculatorImpl.java
@@ -26,6 +26,8 @@ public class CalculatorImpl implements Calculator {
                 if (Double.isInfinite(c))
                     throw new CalculatorException("Division by zero");
                 return c;
+            case modulo:
+                return a % b;
             case mul:
                 return a * b;
         }
