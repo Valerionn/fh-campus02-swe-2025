5c418c6217834a8f3863222142f7a3ab6af40ea1
Sara Katharina Horvat <01310623@edu.campus02.at>
2024-12-14 16:45:44 +0100

    Closes #6 Closes #7 Closes #8

--- src/main/java/at/campus02/swe/parser/Parser.java
+++ src/main/java/at/campus02/swe/parser/Parser.java
@@ -80,6 +80,11 @@ public class Parser {
             return Operation.sub;
         else if ("%".equals(value))
             return Operation.modulo;
+        else if ("sin".equals(value))
+            return Operation.sin;
+        else if ("cos".equals(value))
+            return Operation.cos;
+
 
         throw new CalculatorException("Unsupported Operation!");
     }
